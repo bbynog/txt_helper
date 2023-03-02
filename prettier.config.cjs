776397762b9fc196a091f430412b574047669129
Gabriel <bbynogueira@gmail.com>
2023-03-02 18:22:11 -0300

    Added autogenerated name feature. Added prettier config. Code refactoring

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always'
+};
