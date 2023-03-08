9ed53eaab7e22ec758bb23e373443d5d8884665c
Gabriel <bbynogueira@gmail.com>
2023-03-08 18:09:39 -0300

    Fixed path of txt files

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,5 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.WRITE_PATH_DIR = void 0;
-exports.WRITE_PATH_DIR = `./src/txt_cache`;
+exports.WRITE_PATH_DIR = `./src/txt`;
 //# sourceMappingURL=config.js.map
